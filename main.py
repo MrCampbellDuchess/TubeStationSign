70531763f9fffd93bb48c5bf3f8728c3f1199452
Jon Campbell <jocampbell@sd57.bc.ca>
2024-02-18 14:47:51 -0800

    clears screen and refreshes each minute

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import json
 import requests
 import config
+import os
+import time
 
 app_key = config.app_key
 app_id = config.app_id
 
 url = "https://api.tfl.gov.uk/StopPoint/940GZZLUNFD/Arrivals"
 hdr = {'Cache-Control':'no-cache'}
+
+def convertToMinutes(seconds):
+    minutes = round(seconds/60,0)
+    return minutes
 try:
     r = requests.get(url, headers=hdr)
     response = r.json()
     t1 = (response[0]['timeToStation'])
     t2 = (response[1]['timeToStation'])
     t3 = (response[2]['timeToStation'])
-
-    print("The next train will arrive in " + str(t1) + " seconds.")
+    os.system("cls")
+    print("The next train will arrive in " + str(convertToMinutes(t1)) + " minutes.")
+    print("The next train will arrive in " + str(convertToMinutes(t2)) + " minutes.")
+    print("The next train will arrive in " + str(convertToMinutes(t3)) + " minutes.")
+    time.sleep(60)
 except Exception as e:
     print(e)
