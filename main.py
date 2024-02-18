9381bc2048d0604f108e8099a1f0660b06817b04
Jon Campbell <jocampbell@sd57.bc.ca>
2024-02-18 15:09:31 -0800

    reads station data from config, displays destination and 4 trains

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+
 import requests
 import config
 import os
@@ -6,25 +6,35 @@ import time
 
 app_key = config.app_key
 app_id = config.app_id
+station = config.stationID
 
-url = "https://api.tfl.gov.uk/StopPoint/940GZZLUNFD/Arrivals"
+url = ("https://api.tfl.gov.uk/StopPoint/{}/Arrivals".format(station))
 hdr = {'Cache-Control':'no-cache'}
 
 def convertToMinutes(seconds):
     minutes = round(seconds/60,0)
     return minutes
 
+def countdown(i):
+    while i > 0:
+        print("Refreshing in {} seconds".format(i))
+        time.sleep(1)
+        i -= 1
+        print ("\033[A \033[A")
+
 while True:
     try:
         r = requests.get(url, headers=hdr)
         response = r.json()
-        t1 = (response[0]['timeToStation'])
-        t2 = (response[1]['timeToStation'])
-        t3 = (response[2]['timeToStation'])
+        t1 = [response[0]['towards'], (convertToMinutes(response[0]['timeToStation']))]
+        t2 = [response[1]['towards'], (convertToMinutes(response[1]['timeToStation']))]
+        t3 = [response[2]['towards'], (convertToMinutes(response[2]['timeToStation']))]
+        t4 = [response[2]['towards'], (convertToMinutes(response[3]['timeToStation']))]
         os.system("cls")
-        print("The next train will arrive in " + str(convertToMinutes(t1)) + " minutes.")
-        print("The next train will arrive in " + str(convertToMinutes(t2)) + " minutes.")
-        print("The next train will arrive in " + str(convertToMinutes(t3)) + " minutes.")
-        time.sleep(60)
+        print("The next train towards {} will arrive in {} minutes.".format(t1[0], t1[1]))
+        print("The next train towards {} will arrive in {} minutes.".format(t2[0], t2[1]))
+        print("The next train towards {} will arrive in {} minutes.".format(t3[0], t3[1]))
+        print("The next train towards {} will arrive in {} minutes.".format(t4[0], t4[1]))
+        countdown(60)
     except Exception as e:
         print(e)
